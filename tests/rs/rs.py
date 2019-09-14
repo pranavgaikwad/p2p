89e782720da873efdcf238cda5f74d3a8db4c6cc
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-09-14 17:53:26 -0400

    added rs stubs

---
+++ tests/rs/rs.py
@@ -0,0 +1,27 @@
+import unittest
+
+class RegistrationServerTest(unittest.TestCase): 
+    """ Registration Server Tests"""
+    def test_register(self):
+        """ registers a new client """
+        pass
+    
+    def test_unregister(self):
+        """ un-registers a new client """
+        pass
+
+    def test_start(self):
+        """ starts the server """
+        pass
+
+    def test_stop(self):
+        """ stops the server """
+        pass
+
+    def test_send(self):
+        """ sends a new message """
+        pass
+
+    def test_receive(self):
+        """ receives a new message """
+        pass
