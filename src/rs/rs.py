7c043b3b1ea30745d035951dc37df8047ff047a6
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-09-14 17:58:28 -0400

    added rs stubs

---
+++ src/rs/rs.py
@@ -0,0 +1,30 @@
+
+class RegistrationServer:
+    """ Registration Server """
+    def __init__(self):
+        self.clients = []
+        self.buffer:bytes = []
+
+    def register(self):
+        """ registers a new client """
+        pass
+    
+    def unregister(self):
+        """ un-registers a new client """
+        pass
+
+    def start(self):
+        """ starts the server """
+        pass
+
+    def stop(self):
+        """ stops the server """
+        pass
+
+    def send(self):
+        """ sends a new message """
+        pass
+
+    def receive(self):
+        """ receives a new message """
+        pass
