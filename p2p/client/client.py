1fe2e8d5a87e9ad617e9546232768f268ad581e1
jaymin.desai <jaymindesai766@gmail.com>
2019-09-20 15:59:29 -0400

    RFC Changes - load rfcs in memory, take initial state and goal state as constructor args in P2PServer

--- p2p/client/client.py
+++ p2p/client/client.py
@@ -1,26 +1,44 @@
 from socket import socket, AF_INET, SOCK_STREAM
 import random
+from glob import glob
+from datetime import datetime
 from p2p.utils.logger import logger
 from p2p.server.server import Server
-from p2p.proto.proto import Message, MethodTypes, Headers, ServerResponse, ResponseStatus, ForbiddenError
-from p2p.utils.app_constants import RS_PORT
+from p2p.proto.proto import ResponseStatus as Status
+from p2p.proto.proto import ServerResponse as Response
+from p2p.proto.proto import Message, MethodTypes, Headers, ForbiddenError
+from p2p.utils.app_constants import RS_PORT, RFC_PATH, GOAL_RFC_STATE
 from threading import Lock
 
 
 class Peer:
     def __init__(self):
+        # self.server = P2PServer()
+        # self.client = P2PClient()
         pass
 
 
 class P2PServer(Server):
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, rfc_index, goal_rfc_state=GOAL_RFC_STATE):
         super().__init__(host, port)
         self.mutex = Lock()
         self.cookie = -1
+        self.rfc_index = rfc_index  # set[str] containing rfc indices or initial state
+        self.goal_state = goal_rfc_state
+        self.rfc_data = {}
+
+    def _load_rfcs(self):
+        for file in glob(RFC_PATH + "/*.txt"):
+            idx = file.split('/')[-1][3:7]
+            if idx in self.rfc_index:
+                self.rfc_data[idx] = open(file).read()
+        self.logger("Loaded {} RFCs".format(len(self.rfc_data)))
 
     def _on_start(self):
-        """ register this peer """
+        """ load RFCs in memory and register this peer """
+        self._load_rfcs()
+
         with socket(AF_INET, SOCK_STREAM) as conn:
             conn.connect(('127.0.0.1', RS_PORT))
 
@@ -32,7 +50,7 @@ class P2PServer(Server):
 
             try:
                 conn.send(msg.to_bytes())
-                response = ServerResponse().from_str(conn.recv(1024).decode("utf-8"))
+                response = Response().from_str(conn.recv(1024).decode("utf-8"))
                 self.cookie = response.headers[Headers.Cookie.name]
                 self.logger.info("Peer registered")
             except Exception as e:
@@ -51,7 +69,7 @@ class P2PServer(Server):
 
             try:
                 conn.send(msg.to_bytes())
-                response = ServerResponse().from_str(conn.recv(1024).decode("utf-8"))
+                response = Response().from_str(conn.recv(1024).decode("utf-8"))
                 if int(response.status) == 403:
                     raise ForbiddenError(response.payload)
                 self.logger.info("TTL extended")
@@ -60,9 +78,41 @@ class P2PServer(Server):
             except Exception as e:
                 self.logger.error("Error while extending TTL: {}".format(e))
 
+    def _handle_rfcquery(self, conn, msg):
+        pass
+
+    def _handle_getrfc(self, conn, msg):
+        pass
+
     def _new_message_callback(self, conn, msg):
         """ processes a message """
-        pass
+        p2pmsg = Message()
+        start_time = datetime.now()
+        response = Response(response=Status.Unknown.name, status=Status.Unknown.value)
+        try:
+            p2pmsg.from_str(msg.decode('utf-8'))
+
+            def handle(method):
+                return {
+                    MethodTypes.RFCQuery.name: self._handle_rfcquery,
+                    MethodTypes.GetRFC.name: self._handle_getrfc,
+                }[method]
+
+            response = handle(p2pmsg.method)(conn, p2pmsg)
+        except KeyError:
+            self.logger.error("Method not allowed {}".format(p2pmsg.method))
+            response.payload = "Method not allowed"
+            response.status = Status.MethodNotAllowed.value
+        except Exception as e:
+            self.logger.error("Error processing message {}".format(e))
+            response.payload = "Unknown error encountered"
+            response.status = Status.Unknown.value
+        finally:
+            self.logger.info(
+                "Took {} ms to process request {}".format((datetime.now() - start_time).microseconds / 1000,
+                                                          p2pmsg.method))
+            # send some message back to the client no matter what
+            self.messages[conn].put(response.to_bytes())
 
 
 class P2PClient(object):
@@ -116,7 +166,7 @@ class ClientEntry(object):
 
 
 if __name__ == '__main__':
-    peer = P2PServer("127.0.0.1", random.randint(9900, 9990))
+    peer = P2PServer("127.0.0.1", random.randint(65400, 65500))
     try:
         peer.start()
     except Exception as err:
--- p2p/utils/app_constants.py
+++ p2p/utils/app_constants.py
@@ -1,1 +1,7 @@
+import os
+from pathlib import Path
+
 RS_PORT = 9998
+
+RFC_PATH = os.path.join(Path(__file__).parent.parent.parent, "rfcs")
+GOAL_RFC_STATE = set([str(n) for n in range(1001, 1061)])
