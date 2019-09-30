210ce82f19b123499818aff43af58a206d8350e4
jaymin.desai <jaymindesai766@gmail.com>
2019-09-30 18:04:08 -0400

    package data

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='P2P',
     version='0.0-dev',
     packages=['p2p', 'p2p.proto', 'p2p.server', 'p2p.utils', 'p2p.client'],
-    package_data={'': ['rfcs']},
+    package_data={'p2p': ['rfcs/*.txt']},
     include_package_data=True,
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
